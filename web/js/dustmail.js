a8469f8a81fbd86f908bf370cb1de4551d79976b
Anoop S <anoop.santhanam@gmail.com>
2017-12-29 13:41:14 +0530

    Adding form

--- web/js/dustmail.js
+++ web/js/dustmail.js
@@ -4,7 +4,7 @@ app.controller("mail",function($scope,$compile,$http){
     $scope.loadMap=function(){
         var mapProp= {
             center:new google.maps.LatLng(51.508742,-0.120850),
-            zoom:1,
+            zoom:3,
         };
         $scope.map=new google.maps.Map(document.getElementById("map"),mapProp);
     };
