13acfa2ef34a8d7daae509e3c5cc563f3b6e6f94
Anoop S <anoop.santhanam@gmail.com>
2017-12-29 10:32:02 +0530

    First setup

---
+++ web/index.php
@@ -0,0 +1,35 @@
+<?php
+
+ini_set('display_errors', 1);
+require_once __DIR__.'/../vendor/autoload.php';
+$app = require __DIR__.'/../src/app.php';
+require __DIR__.'/../config/prod.php';
+require __DIR__.'/../src/controllers.php';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => 'php://stderr',
+));
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+      'driver' => 'pdo_mysql',
+      'dbname' => 'database',
+      'user' => 'user',
+      'password' => 'password',
+      'host'=> "www.host.net",
+    )
+));
+$app->register(new Silex\Provider\SessionServiceProvider, array(
+    'session.storage.save_path' => dirname(__DIR__) . '/tmp/sessions'
+));
+$app->before(function(Request $request) use($app){
+    $request->getSession()->start();
+});
+$app->get("/",function() use($app){
+    $app['twig']->render("index.html.twig");
+});
+$app->run();
+?>
