b16c7fa76e6c2b284e94734cc027ab51e3ee5e81
Anoop S <anoop.santhanam@gmail.com>
2017-12-30 11:43:29 +0530

    Adding credentials

--- web/js/dustmail.js
+++ web/js/dustmail.js
@@ -14,6 +14,18 @@ app.controller("mail",function($scope,$compile,$http){
             height: height+"px"
         });
         $("#form").css("height",(height-51)+"px");
+        var text='<table width="100%" cellpadding=0 cellspacing=10 align="center">';
+        for(var i=1;i<=15;i++){
+            if((i==1)||(i==6)||(i==11)){
+                text+='<tr>';
+            }
+            text+='<td align="center" width="20%"><img src="images/credentials/'+i+'.png" class="img-responsive" style="width:80px;"></td>';
+            if((i==5)||(i==11)||(i==15)){
+                text+='</tr>';
+            }
+        }
+        text+='</table>';
+        $("#credlist").html(text);
     };
 });
 window.resize=function(){
