db3f6953e23e0db767e64a907cf848f913aaf4eb
Anoop S <anoop.santhanam@gmail.com>
2017-12-31 15:45:53 +0530

    Handling errors

--- web/js/ui.js
+++ web/js/ui.js
@@ -87,6 +87,20 @@ function mover(layer){
 	$(".form").removeAttr("active");
 	$(layer).attr("active","true");
 }
+var getUrlParameter = function getUrlParameter(sParam) {
+	var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+			sURLVariables = sPageURL.split('&'),
+			sParameterName,
+			i;
+
+	for (i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split('=');
+
+			if (sParameterName[0] === sParam) {
+					return sParameterName[1] === undefined ? true : sParameterName[1];
+			}
+	}
+};
 function readParams(){
 	var err=getUrlParameter('err');
 	if(validate(err)){
