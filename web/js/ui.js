98452d168c209f4a1d24aafa45481bd286ba4d3a
Anoop S <anoop.santhanam@gmail.com>
2017-12-31 15:43:45 +0530

    Handling errors

--- web/js/ui.js
+++ web/js/ui.js
@@ -87,3 +87,42 @@ function mover(layer){
 	$(".form").removeAttr("active");
 	$(layer).attr("active","true");
 }
+function readParams(){
+	var err=getUrlParameter('err');
+	if(validate(err)){
+		switch(err){
+			case "INVALID_PARAMETERS":
+			default:
+			err='Something went wrong while processing your request.';
+			break;
+			case "INVALID_USER_CREDENTIALS":
+			err='Invalid credentials. Please verify the details and try again.';
+			break;
+			case "INVALID_USER_NAME":
+			err='Invalid user name. Please enter your full name and try again.';
+			break;
+			case "INVALID_USER_EMAIL":
+			err='Invalid email ID. Please enter a valid email ID and try again.';
+			break;
+			case "INVALID_PASSWORD":
+			err='Invalid password. Please ensure the password is at least 8 characters in length.';
+			break;
+			case "PASSWORD_MISMATCH":
+			err='Password mismatch. Please ensure the passwords match each other.';
+			break;
+		}
+		$("#message").html('<div class="alert alert-danger"><strong>Error</strong> '+err+'</div>');
+	}
+	var suc=getUrlParameter("suc");
+	if(validate(suc)){
+		switch(suc){
+			case "ACCOUNT_CREATED":
+			suc='Account created successfully. You may login to your account now.';
+			break;
+			case "ACCOUNT_VERIFIED":
+			suc='Your account was verified successfully. Login to continue.';
+			break;
+		}
+		$("#message").html('<div class="alert alert-success"><strong>Success</strong> '+suc+'</div>');
+	}
+}
