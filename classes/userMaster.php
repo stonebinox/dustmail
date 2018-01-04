8e88d36acfdfcf7c9e55fd8a7fcb2ce3da21022d
Anoop S <anoop.santhanam@gmail.com>
2018-01-04 11:18:21 +0530

    Added field for resetting password

--- classes/userMaster.php
+++ classes/userMaster.php
@@ -95,6 +95,7 @@ class userMaster extends adminMaster
             {
                 $um['admin_master_idadmin_master']=$admin;
             }
+            var_dump($um);
             return $um;
         }
         else
