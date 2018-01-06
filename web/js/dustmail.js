6ae5bf59222bf0a2f59b2eaf6dce58955b50daa9
Anoop S <anoop.santhanam@gmail.com>
2018-01-06 11:39:46 +0530

    Added coupon code functionality

--- web/js/dustmail.js
+++ web/js/dustmail.js
@@ -312,7 +312,7 @@ app.controller("mail",function($scope,$compile,$http){
                 $(form).append('<div class="alert alert-success"><strong>Coupon Applied</strong> The coupon code has been applied! '+text+'</div>');
             }
             amount=amount*100;
-            $(form).append('<script src="https://checkout.stripe.com/checkout.js" class="stripe-button" data-key="pk_live_PSwDNF9BVOIuKl5BBzVXnXsa" data-amount="'+amount+'" data-name="Dust &amp; Co., Inc." data-description="Widget" data-image="https://stripe.com/img/documentation/checkout/marketplace.png" data-locale="auto"></script>');
+            $(form).append('<script src="https://checkout.stripe.com/checkout.js" class="stripe-button" data-key="pk_live_PSwDNF9BVOIuKl5BBzVXnXsa" data-amount="'+amount+'" data-name="Dust &amp; Co., Inc." data-description="Widget" data-image="images/dust-logo.png" data-locale="auto"></script>');
             messageBox("Make Payment",form);
         }
         else{
